6934d21d65ef571aa99de28dc8800575070c77d1
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-10-25 15:56:22 -0300

    Implementado função jogar no JS e melhorias no css

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Regras do jogo:
 
 - O jogo tem 2 jogadores, jogando em turnos
 - Em cada turno, o jogador joga o dado quantas vezes quiser. Cada resultado é adicionado a sua pontuação da rodada
-- MAS, se o dado cair com o n° 1, todo os pontos da rodada será perdido e passará a vez para o outro jogador
-- O jogador pode escolher "Passa a vez", que adicionará os pontos da rodada a seu placar e passará a vez para o outro jogador
+- MAS, se o dado mostrar o n° 1, todo os pontos da rodada serão perdidos e passará a vez para o outro jogador
+- O jogador pode escolher "Passa a vez", que adicionará os pontos ganhos na rodada a seu placar e passará a vez para o outro jogador
 - O primeiro jogador que alcançar 100 pontos no placar ganha o jogo
--- scripts.js
+++ scripts.js
@@ -1,2 +1,48 @@
 "use strict";
 
+// DOM
+const diceDom = document.querySelector(".dice");
+const player0 = document.querySelector(".player-0");
+const player1 = document.querySelector(".player-1");
+const score0 = document.querySelector("#score-0");
+const score1 = document.querySelector("#score-1");
+const current0 = document.querySelector("#current-0");
+const current1 = document.querySelector("#current-1");
+const btnPlay = document.querySelector(".btn-play");
+const btnHold = document.querySelector(".btn-hold");
+const btnNew = document.querySelector(".btn-new");
+
+// Valores iniciais
+const initValues = function() {
+    diceDom.classList.add("d-none");
+    score0.textContent = 0;
+    score1.textContent = 0;
+    current0.textContent = 0;
+    current1.textContent = 0;
+}
+
+const scores = [0, 0]
+let currentScore = 0;
+let currentPlayer= 0;
+
+// Inicializando
+initValues();
+
+// Jogar dado
+btnPlay.addEventListener("click", function() {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceDom.src = `dado-${dice}.png`;
+    diceDom.classList.add("d-block");
+
+    if (dice > 1) {
+        currentScore += dice;
+        document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
+    } else {
+        currentScore = 0
+        document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
+        // e passa a vez
+        currentPlayer = currentPlayer === 0 ? 1 : 0;
+        player0.classList.toggle("active");
+        player1.classList.toggle("active");
+    }
+});
