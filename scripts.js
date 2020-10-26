ddc1895521bd31ae1e2360d3a2d0acbbcae830b2
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-10-26 14:52:11 -0300

    Projeto finalizado

--- scripts.js
+++ scripts.js
@@ -1,59 +1,77 @@
 "use strict";
 
-// DOM
-const diceDom = document.querySelector(".dice");
-const player0 = document.querySelector(".player-0");
-const player1 = document.querySelector(".player-1");
-const score0 = document.querySelector("#score-0");
-const score1 = document.querySelector("#score-1");
-const current0 = document.querySelector("#current-0");
-const current1 = document.querySelector("#current-1");
-const btnPlay = document.querySelector(".btn-play");
-const btnHold = document.querySelector(".btn-hold");
-const btnNew = document.querySelector(".btn-new");
+(function (){
+    // DOM
+    const dice = document.querySelector(".dice");
+    const btnPlay = document.querySelector(".btn-play");
+    const btnHold = document.querySelector(".btn-hold");
+    const btnNew = document.querySelector(".btn-new");
 
-// Valores iniciais
-const initValues = function() {
-    diceDom.classList.add("d-none");
-    score0.textContent = 0;
-    score1.textContent = 0;
-    current0.textContent = 0;
-    current1.textContent = 0;
-}
+    // Variáveis utilizadas
+    let scores, playing, currentScore, currentPlayer;
 
-const scores = [0, 0]
-let currentScore = 0;
-let currentPlayer= 0;
-
-// Inicializando
-initValues();
-
-const switchPlayer = function() {
-    currentScore = 0
-    document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
-    // e passa a vez
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    player0.classList.toggle("active");
-    player1.classList.toggle("active");
-}
-
-// Passar a vez
-btnHold.addEventListener("click", function() {
-    scores[currentPlayer] += currentScore;
-    document.querySelector(`#score-${currentPlayer}`).textContent = scores[currentPlayer];
-    switchPlayer();
-});
-
-// Jogar dado
-btnPlay.addEventListener("click", function() {
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    diceDom.src = `dado-${dice}.png`;
-    diceDom.classList.add("d-block");
-
-    if (dice > 1) {
-        currentScore += dice;
-        document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
-    } else {
-        switchPlayer();
+    // Inicializa valores
+    initValues();
+    const initValues = function() {
+        scores = [0, 0];
+        playing = true;
+        currentScore = 0;
+        currentPlayer = 0;
+        document.querySelector("#score-0").textContent = 0;
+        document.querySelector("#score-1").textContent = 0;
+        document.querySelector("#current-0").textContent = 0;
+        document.querySelector("#current-1").textContent = 0;
+        dice.classList.add("d-none");
+        document.querySelector(`#name-0`).textContent = "Player 1";
+        document.querySelector(`#name-1`).textContent = "Player 2";
+        document.querySelector(`.player-0`).classList.remove("winner");
+        document.querySelector(`.player-1`).classList.remove("winner");
+        document.querySelector(`.player-0`).classList.add("active");
+        document.querySelector(`.player-1`).classList.remove("active");
     }
-});
+
+    // Novo jogo
+    btnNew.addEventListener("click", initValues);
+
+    // Joga o dado
+    btnPlay.addEventListener("click", function() {
+        if (playing) {
+            const diceNumber = Math.trunc(Math.random() * 6) + 1;
+            dice.src = `dado-${diceNumber}.png`;
+            dice.classList.add("d-block");
+            if (diceNumber > 1) {
+                currentScore += diceNumber;
+                document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
+            } else {
+                switchPlayer();
+            }
+        }
+    });
+
+    // Passa a vez
+    btnHold.addEventListener("click", function() {
+        if (playing) {
+            scores[currentPlayer] += currentScore;
+            document.querySelector(`#score-${currentPlayer}`).textContent = scores[currentPlayer];
+            // Verifica se o jogador atingiu 100 pontos
+            if (scores[currentPlayer] >= 30) {
+                playing = false;
+                document.querySelector(`#name-${currentPlayer}`).textContent = "🥇 WINNER";
+                document.querySelector(`.player-${currentPlayer}`).classList.toggle("winner");
+                dice.classList.remove("d-block");
+            } else {
+                switchPlayer();
+            }
+        }
+    });
+
+    // Troca de jogador
+    const switchPlayer = function() {
+        currentScore = 0
+        document.querySelector(`#current-${currentPlayer}`).textContent = currentScore;
+        currentPlayer = currentPlayer === 0 ? 1 : 0;
+        document.querySelector(".player-0").classList.toggle("active");
+        document.querySelector(".player-1").classList.toggle("active");
+        dice.classList.remove("d-block");
+    }
+})();
